cc110463f664c61f63b181ff0bd8acb9df676997
Francisco Anselmo <francopilloto@gmail.com>
2025-07-08 12:35:12 -0300

    refactor(auth): update RefreshTokenStrategy to use AuthAccount
    - RefreshTokenStrategy validates token using AuthAccount and associated hashed refresh token

--- src/modules/auth/strategies/jwt-refresh.strategy.ts
+++ src/modules/auth/strategies/jwt-refresh.strategy.ts
@@ -7,10 +7,10 @@ import { Request } from 'express';
 import { Strategy } from 'passport-jwt';
 import { Repository } from 'typeorm';
 
-import { UserService } from '@modules/user/user.service';
 import { fk } from '@utils/db';
 
-import { AuthStrategyName } from '../auth.constants';
+import { AuthStrategyName } from '../auth.enums';
+import { AuthAccount } from '../entities/auth-account.entity';
 import { AuthToken } from '../entities/token.entity';
 import { TokenFactory } from '../token/token.factory';
 import { jwtFromRequest } from '../token/token.utils';
@@ -19,10 +19,10 @@ import { jwtFromRequest } from '../token/token.utils';
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, AuthStrategyName.REFRESH_JWT) {
   constructor(
     readonly configService: ConfigService,
-
+    @InjectRepository(AuthAccount)
+    private readonly accountRepo: Repository<AuthAccount>,
     @InjectRepository(AuthToken)
     private readonly tokenRepo: Repository<AuthToken>,
-    private readonly userService: UserService,
     private readonly tokenFactory: TokenFactory,
   ) {
     super({
@@ -33,24 +33,24 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, AuthStrateg
     });
   }
 
-  async validate(req: Request, { sub: userId, aud: device }: { sub: string; aud: string }) {
+  async validate(req: Request, { sub: accountId, aud: device }: { sub: string; aud: string }) {
     const refreshToken = jwtFromRequest(req);
 
-    const [user, token] = await Promise.all([
-      this.userService.findOneById(userId),
+    const [account, token] = await Promise.all([
+      this.accountRepo.findOne({ where: fk(accountId), relations: ['user'] }),
       this.tokenRepo.findOne({
-        where: { user: fk(userId), device },
+        where: { account: fk(accountId), device },
         select: ['id', 'refreshTokenHash'],
       }),
     ]);
 
     if (
-      user &&
+      account &&
       token &&
       refreshToken &&
       token.refreshTokenHash === this.tokenFactory.hashToken(refreshToken)
     ) {
-      return [user, { device }];
+      return [account.user ?? null, { account, device }];
     }
 
     throw new UnauthorizedException('Invalid refresh token');
